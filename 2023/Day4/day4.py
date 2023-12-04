34beb7d8e2276ddf7f259a1c82d7cdc6eae4c68a
rishabha25 <rishabh.a25@gmail.com>
2023-12-04 16:51:26 +0000

    part 2 using recursion; try better approach

--- 2023/Day4/day4.py
+++ 2023/Day4/day4.py
@@ -30,3 +30,27 @@ with open("2023/Day4/input.txt") as file:
 
 print (my_l[0])
 print(f"The total sum of card pile scores is: {cu_sum_card_power}")
+
+# Part 2
+# Use Recursion
+# Dict to keep track of numbers of copies
+
+dict_count_cards = dict([(i+1, 1) for i in range(len(winning_l))])
+
+def process_card(card):
+    card_match = 0
+    for my_num in my_l[card-1]:
+        if my_num in winning_l[card-1]:
+            card_match += 1
+    
+    for i in range(card_match):
+        dict_count_cards[card+i+1] = dict_count_cards[card+i+1]+1
+        process_card(card+i+1)
+
+    return
+
+for i in range(len(my_l)):
+    process_card(i+1)
+                
+
+print(sum(dict_count_cards.values()))
